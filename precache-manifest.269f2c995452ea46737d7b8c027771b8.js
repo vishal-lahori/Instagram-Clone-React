1dc1bca525911ab11a08a10ff6f235dcf0d47767
vishal-lahori <vishallahori1@gmail.com>
2020-09-19 15:32:41 +0530

    Updates

---
+++ precache-manifest.269f2c995452ea46737d7b8c027771b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1526947464c84a1478a3f2484b5f7a31",
+    "url": "/Instagram-Clone-React/index.html"
+  },
+  {
+    "revision": "318e425b0b43fba2a1d2",
+    "url": "/Instagram-Clone-React/static/css/main.9082e93d.chunk.css"
+  },
+  {
+    "revision": "240f19c333a21c12af4a",
+    "url": "/Instagram-Clone-React/static/js/2.6d85803c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Instagram-Clone-React/static/js/2.6d85803c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "318e425b0b43fba2a1d2",
+    "url": "/Instagram-Clone-React/static/js/main.33f3e4c2.chunk.js"
+  },
+  {
+    "revision": "563d13eb6e0fef5f2b9c",
+    "url": "/Instagram-Clone-React/static/js/runtime-main.35354052.js"
+  }
+]);
